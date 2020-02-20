503db88ed2ce5d70c518f1665904a2fc0a19b8d2
Jkresnak <cmstudent@uhwo-acm-01.uhwo.hawaii.edu>
2020-02-20 10:37:32 -1000

    Completed Parts 1-8 and Problem Set 1

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var TopLabel: UILabel!
     
     @IBOutlet weak var BottomLabel: UILabel!
+    @IBOutlet weak var ButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.TopLabel.text = "My Favorite Foods"
@@ -20,12 +22,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     var currentIndex = 0
-    var FoodNames = ["Ice cream", "Pizza", "Mac & cheese", "Chocolate", "Hot dogs"]
+    var favoriteFoodArray = ["Ice cream", "Pizza", "Mac & cheese", "Chocolate", "Hot dogs"]
     
    
-   
-    @IBAction func ButtonPressed(_ sender: UIButton) {
-        self.BottomLabel.text = FoodNames[0]
+    //Button is recognized as an IBAction, so we can code what happens when it is pressed.
+        @IBAction func ButtonPressed(_ sender: UIButton)
+    {
+        //Created the Array for the bottom label to print.
+                self.BottomLabel.text = favoriteFoodArray[currentIndex]
+        
+        //Getting the Array ready to be pressed through in the BottomLabel
+        if self.currentIndex < self.favoriteFoodArray.count
+         {
+            
+        //Creating the current index to start the process of clicking through the 5 arrays
+            currentIndex+=1
+            
+        //Takes control of the text of Buttonpressed to change when it is pressed
+            ButtonLabel.setTitle("next", for: UIControl.State.normal)
+        }
+        else
+        {
+        //Disables the button when everything in the array has been accessed.
+            (sender as! UIButton).isEnabled=false
+        }
     }
     
     }
